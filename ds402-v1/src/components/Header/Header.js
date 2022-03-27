ec7da0503db2187115ff1a1c1963d282c9c9cc13
ahmedmmeky <meky.wrk@gmail.com>
2022-03-27 17:43:49 -0400

    added content for header.

--- ds402-v1/src/components/Header/Header.js
+++ ds402-v1/src/components/Header/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
+import styles from "./Header.module.scss";
 
-export const header = () => {
-  return <div>header</div>;
+export const Header = () => {
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.logo}>
+        <h3>DS 402 Project</h3>
+      </div>
+      <div className={styles.navItems}>
+        <h3>Algorithms</h3>
+        <h3>About</h3>
+      </div>
+    </div>
+  );
 };
 
-export default header;
+export default Header;
