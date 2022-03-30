060ed698ed4d3ff0630c336ee4a597e5eb3994e2
ahmedmmeky <meky.wrk@gmail.com>
2022-03-30 17:16:31 -0400

    added add functionality for each group memebr.

--- ds402-v1/src/components/AlgoOne/AlgoOne.js
+++ ds402-v1/src/components/AlgoOne/AlgoOne.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 import styles from "./AlgoOne.module.scss";
 import TextField from "@mui/material/TextField";
 
 export const AlgoOne = () => {
+  const [members, setMembers] = useState([{ name: "", preferences: "" }]);
+
+  const handleAddClicked = () => {
+    setMembers([...members, { name: "", preferences: "" }]);
+  };
   return (
     <div>
       <div className={styles.toolBar}>
         <h4>Group Members</h4>
         <div className={styles.buttonContainer}>
           <RemoveIcon />
-          <AddIcon />
+          <AddIcon onClick={() => handleAddClicked()} />
         </div>
 
         <div className={styles.memberContainer}>
@@ -19,9 +24,13 @@ export const AlgoOne = () => {
             <h4>Name</h4>
             <h4>Preferences</h4>
           </div>
-          <div className={styles.member}>
-            <TextField />
-            <TextField />
+          <div className={styles.members}>
+            {members.map((member, index) => (
+              <div key={index} className={styles.member}>
+                <TextField />
+                <TextField />
+              </div>
+            ))}
           </div>
         </div>
       </div>
